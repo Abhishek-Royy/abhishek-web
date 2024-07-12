2514ef7b2ec7c354191cdfeec196fbae30f1d859
Abhishek-coder2001 <abhishek800.roy@gmail.com>
2024-07-12 14:19:57 +0530

    github acc add

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -78,7 +78,7 @@ function Home() {
 
         {/* SECTION-2 */}
 
-        <div className="sec-2 w-full md:h-[90vh] md:pt-20 bg-gradient-to-br from-black to-[#010815] pt-10  md:px-32 text-white ">
+        <div className="sec-2 w-full h-auto md:pt-20 bg-gradient-to-br from-black to-[#010815] py-10  md:px-32 text-white ">
           <h1
             className="w-max md:hidden  p-3 h-max mx-auto justify-center text-3xl font-bold animate-bounce flex items-center gap-3 text-[#ffe72f] mb-5"
             // style={{
@@ -105,9 +105,40 @@ function Home() {
                 required to perform specific tasks and use specific tools and
                 programs in real world situations.
               </p>
+
+              <a href="https://github.com/Abhishek-Royy">
+                <button
+                  type="button"
+                  className="text-white bg-transparent  focus:outline-none hover:bg-[#EAB308] hover:text-black font-medium rounded-lg text-sm px-5 py-3 text-center inline-flex items-center  me-2 mt-5"
+                  style={{
+                    boxShadow:
+                      " #ffe72f 0px 1px 36px 0px, #f9e447 0px 0px 0px 1px",
+                  }}
+                >
+                  <svg
+                    className="w-4 h-4 me-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  My Github Profile
+                </button>
+              </a>
             </div>
           </div>
         </div>
+
+
+        {/* SECTION-3 */}
+
+        
       </div>
     </>
   );
